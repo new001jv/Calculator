d5dc4337f383fb7ea8c21e45aaf09ab2903e4b01
Justin <jv573@nau.edu>
2016-07-15 14:55:35 -0700

    added slider

--- src/Calculator/wwwroot/js/site.js
+++ src/Calculator/wwwroot/js/site.js
@@ -185,3 +185,16 @@ function Plot(input) {
         }]
     });
 };
+
+$(function () {
+    $("#slider-range-max").slider({
+        range: "max",
+        min: 1,
+        max: 10,
+        value: 2,
+        slide: function (event, ui) {
+            $("#amount").val(ui.value);
+        }
+    });
+    $("#amount").val($("#slider-range-max").slider("value"));
+});
